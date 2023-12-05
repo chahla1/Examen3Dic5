2f7d91442461eb52d04eed5398c69e1a9775a45e
chahla1 <cbouchot@myuax.com>
2023-12-05 11:14:32 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 class Environment {
 private:
-    map<string, int> symbol_Map;
+    map<string, int> symbolMap;
 public:
-    Environment(){}
-    Variant lookup(const string& symbolName) const {
-        auto it = symbolTable.find(symbolName);
-        if (it != symbolTable.end()) {
-            return it->second;
-        } else {
-            return Variant();
-        }
+    Environment() {
+        addSymbol("pi", 3.1416);
     }
 
+    void addSymbol(const string &symbol, int value) {
+        symbolMap[symbol] = value;
+    }
 
+    int getSymbolValue(const string &symbol) const {
+        auto it = symbolMap.find(symbol);
+        if (it != symbolMap.end()) {
+            return it->second;
+        } else {
+            return 0;
+        }
 
-
-
-
-}
-
-
+    }
+};
