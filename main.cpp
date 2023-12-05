0e18c25e9e4ba51f5684ca301f7f4c21cb414d68
chahla1 <cbouchot@myuax.com>
2023-12-05 10:56:46 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <map>
 #include <string>
+using namespace std;
 
+// 1. Estructura Básica:
+
+class Environment {
+private:
+    std::map<std::string, int> symbol_Map;
+public:
+    Environment(){
+        addSymbol("pi", 3,1416);
+        addSymbol("e", 2,718);
+    }
+
+}
 
 
