744a9416d26562b067ddcfbb4c5ffc7232a75d38
chahla1 <cbouchot@myuax.com>
2023-12-05 11:25:06 +0100

    otro commit

--- main.cpp
+++ main.cpp
@@ -2,28 +2,33 @@
 #include <map>
 #include <string>
 using namespace std;
+#include "Variant.h"
 
 // 1. Estructura Básica:
 
 class Environment {
 private:
-    map<string, int> symbolMap;
+    map<string, Variant> symbolMap;
 public:
     Environment() {
-        addSymbol("pi", 3.1416);
+        addSymbol("pi", Variant(3.1416));
     }
 
-    void addSymbol(const string &symbol, int value) {
+    void addSymbol(const string &symbol, const Variant &value) {
         symbolMap[symbol] = value;
     }
 
-    int getSymbolValue(const string &symbol) const {
+    Variant lookup(const std::string &symbol) const {
         auto it = symbolMap.find(symbol);
         if (it != symbolMap.end()) {
             return it->second;
         } else {
-            return 0;
+            return Variant();
         }
 
     }
 };
+//2. Insertar Símbolos:
+
+
+
